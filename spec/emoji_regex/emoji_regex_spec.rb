3e0ba9467b5b96c064cd13749ff68bca1003d87f
Jessica Stokes <hello@jessicastokes.net>
2017-07-24 11:39:36 -0700

    🌡 Improve specs for ::Text regex

--- spec/emoji_regex/emoji_regex_spec.rb
+++ spec/emoji_regex/emoji_regex_spec.rb
@@ -24,12 +24,24 @@ RSpec.describe EmojiRegex do
   end
 
   describe '::Text' do
-    it "matches \u{2194}" do
+    it "matches ⌚️ default emoji presentation character (Emoji_Presentation)" do
+      expect("\u{231A}".scan(EmojiRegex::Text)).to eql(["\u{231A}"])
+    end
+
+    it "matches ↔ default text representation character" do
       expect("\u{2194}".scan(EmojiRegex::Text)).to eql(["\u{2194}"])
     end
 
-    it "matches \u{1F321}" do
-      expect("\u{1F321}".scan(EmojiRegex::Text)).to eql(["\u{1F321}"])
+    it "matches ↔️ default text presentation character rendered as emoji" do
+      expect("\u{2194}\u{FE0F}".scan(EmojiRegex::Text)).to eql(["\u{2194}\u{FE0F}"])
+    end
+
+    it "matches 👩 emoji modifier base (Emoji_Modifier_Base)" do
+      expect("\u{1F469}".scan(EmojiRegex::Text)).to eql(["\u{1F469}"])
+    end
+
+    it "matches 👩🏿 emoji modifier base followed by a modifier" do
+      expect("\u{1F469}\u{1F3FF}".scan(EmojiRegex::Text)).to eql(["\u{1F469}\u{1F3FF}"])
     end
 
     it "doesn't match non-emojis" do
