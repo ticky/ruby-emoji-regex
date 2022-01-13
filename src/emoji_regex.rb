fe2cadc9d6b5bf2a03722943eaea021d4d6437ca
Jessica Stokes <hello@jessicastokes.net>
2022-01-13 12:51:58 -0800

    Update description of EmojiRegex::Regex

--- src/emoji_regex.rb
+++ src/emoji_regex.rb
@@ -1,5 +1,5 @@
 module EmojiRegex
-  # Matches emoji which are recommended for general interchange, as defined by the `RGI_Emoji` property in the Unicode standard.
+  # Matches characters which are emoji, as defined by the Unicode standard's `emoji-test` data file, https://unicode.org/Public/emoji/14.0/emoji-test.txt
   #
   # "#️⃣" (U+0023,U+FE0F,U+20E3) is matched, but not "#️" (U+0023,U+FE0F) or "#" (U+0023).
   Regex = /% emojiRegex %/
